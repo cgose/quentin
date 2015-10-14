f021537c4d43b71501fb7826ffbecbda4a01acf3
U-BSK\cgose <cgose@IS7_CGOSE.BSK.local>
2015-10-14 16:58:03 -0700

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quentinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the quentinApp
+ */
+angular.module('quentinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quentinApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quentinApp
+ */
+angular.module('quentinApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
