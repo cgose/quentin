45abd2bc29043d57adefe9b07f50da3a532f50f2
Cameron Gose <cgose001@gmail.com>
2015-10-15 14:06:12 -0700

    Added More Character Stats and proficiencies. Made a test object to test layout with.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,20 @@ angular.module('quentinApp')
       'AngularJS',
       'Karma'
     ];
+    this.Character = {
+	PlayerName: "Cameron Gose",
+	Race: "Human",
+	Classes: [ { Level: 3, ClassName: "Fighter", HD: 'd10' }, { Level: 1, ClassName: "Wizard", HD: 'd6'}],
+        TotalLevel: 4,
+	Experience: null,
+	ProficiencyBonus: 2,
+	Strength: 16,
+	Dexterity: 8,
+	Constitutions: 14,
+	Wisdom: 12,
+	Intelligence: 16,
+	Charisma: 8,
+	AC: 19,
+	Speed: 30
+    }
   });
