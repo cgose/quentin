560e13fe8a2734edc8c44a065f7cca7130da075e
Cameron Gose <cgose001@gmail.com>
2015-10-16 16:59:57 -0700

    Added more styling and organization

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,19 +15,23 @@ angular.module('quentinApp')
       'Karma'
     ];
     this.Character = {
-	PlayerName: "Cameron Gose",
-	Race: "Human",
-	Classes: [ { Level: 3, ClassName: "Fighter", HD: 'd10' }, { Level: 1, ClassName: "Wizard", HD: 'd6'}],
-        TotalLevel: 4,
-	Experience: null,
-	ProficiencyBonus: 2,
-	Strength: 16,
-	Dexterity: 8,
-	Constitutions: 14,
-	Wisdom: 12,
-	Intelligence: 16,
-	Charisma: 8,
-	AC: 19,
-	Speed: 30
-    }
+        Name: "Quentin the Carver",
+        PlayerName: "Cameron Gose",
+        Race: "Human",
+        Classes: [ { Level: 3, ClassName: "Fighter", HD: 'd10' }, { Level: 1, ClassName: "Wizard", HD: 'd6'}],
+            TotalLevel: 4,
+        Experience: null,
+        ProficiencyBonus: 2,
+        Strength: 16,
+        Dexterity: 8,
+        Constitution: 14,
+        Wisdom: 12,
+        Intelligence: 16,
+        Charisma: 8,
+        AC: 19,
+        Speed: 30,
+        CurrentHP: 37,
+        TotalHP: 37,
+        
+    };
   });
